4e7f4858293017a9d3bac49e3005dbc30d2008e6
Cristian Lungu <lungu.cristian@gmail.com>
2016-03-02 11:42:33 +0200

    Added tema1 implamentation with classes.

--- tema1/main.cpp
+++ tema1/main.cpp
@@ -2,17 +2,147 @@
 
 #include "utils.h"
 
-void insertion_sort(int *A, int size);
-void bubble_sort(int *A, int size);
+
+struct Count {
+    int compare = 0;
+    int assign = 0;
+};
+
+void insertion_sort(int *A, int size, Count &counter);
+
+void bubble_sort(int *A, int size, Count &count);
+
 void selection_sort(int *A, int size);
 
 void exchange(int &a, int &b);
+
 int indexOfMinimum(int first, int end, int *A);
 
 void assertAscending(const int *rnd, int size);
+
 void print(int *array, int size);
 
+
 using namespace std;
+
+class Sort {
+protected:
+    Count counter;
+public:
+    virtual void sort(int *A, int size) = 0;
+    virtual int *genBstCase(int size, int min, int max) = 0;
+    virtual int *genWstCase(int size, int min, int max) = 0;
+    virtual Count getCounter() { return counter; };
+    virtual ~Sort() { }
+};
+
+class BubbleSort : public Sort {
+public:
+    /**
+     * [1] "Introduction To Algorithms, 3rd ed." Pag. 40
+     * for i = 1 to A.length - 1
+     *   for j = A.length downto i + 1
+     *      if A[j] < A[j-1]
+     *          exchange A[j] with A[j - 1]
+     */
+    void sort(int *A, int size) {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = size; j >= i + 1; j--) {
+                counter.compare++;
+                if (A[j] < A[j - 1]) {
+                    exchange(A[j], A[j - 1]);
+                    counter.assign += 3;
+                }
+            }
+        }
+    }
+
+    int *genBstCase(int size, int min, int max) {
+        // ?!
+        return NULL;
+    }
+
+    int *genWstCase(int size, int min, int max) {
+        // ?!
+        return NULL;
+    }
+};
+
+class InsertionSort : public Sort {
+public:
+    /**
+     * [2] "Introduction To Algorithms, 3rd ed." Pag. 18
+     * for j = 2 to A.length
+     *      key = A[j]
+     *      //Insert A[j] into sorted sequence A[i...j-1]
+     *      i = j - 1
+     *      while i > 0 and A[i] > key
+     *          A[i+1] = A[i]
+     *          i = i - 1
+     *      A[i+1] = key
+     */
+    void sort(int *A, int size) {
+        int i = 0;
+        for (int j = 1; j < size; j++) {
+            int key = A[j];
+            i = j - 1;
+            while (i > -1 && A[i] > key) {      //be careful when adapting a [1 to n] algo to a [0 to n-1] one!
+                A[i + 1] = A[i];
+                i = i - 1;
+                counter.compare += 1;
+                counter.assign += 1;
+            }
+            A[i + 1] = key;
+            counter.assign += 2;
+        }
+    }
+
+    int *genBstCase(int size, int min, int max) {
+        // ?!
+        return NULL;
+    }
+
+    int *genWstCase(int size, int min, int max) {
+        // ?!
+        return NULL;
+    }
+};
+
+class SelectionSort : public Sort {
+private:
+    int indexOfMinimum(int first, int end, int *A) {
+        int min = first;
+        for (int i = first + 1; i < end; i++) {
+            if (A[i] < A[min]) {
+                min = i;
+            }
+        }
+        return min;
+    }
+
+public:
+    /**
+     * for each element of the array
+     *      search for the minimum element in the array beginning with the current one
+     *      exchange the minimum found with the current one
+     */
+    void sort(int *A, int size) {
+        for (int i = 0; i < size - 1; i++) {
+            int minim = indexOfMinimum(i, size, A);
+            exchange(A[i], A[minim]);
+        }
+    }
+
+    int *genBstCase(int size, int min, int max) {
+        // ?!
+        return NULL;
+    }
+
+    int *genWstCase(int size, int min, int max) {
+        // ?!
+        return NULL;
+    }
+};
 
 int main() {
     int size = 10;
@@ -22,30 +152,84 @@ int main() {
     //Exercise 1    (bubble sort)
     {
         int *rnd = genRandomArray(size, min, max);
-        bubble_sort(rnd, size);
+        Sort* bubble = new BubbleSort();
+        bubble->sort(rnd, size);
         assertAscending(rnd, size);
     }
 
     //Exercise 2    (insertion sort)
     {
         int *rnd = genRandomArray(size, min, max);
-        insertion_sort(rnd, size);
+        Sort* insert = new InsertionSort();
+        insert->sort(rnd, size);
         assertAscending(rnd, size);
     }
 
     //Exercise 3    (selection sort)
     {
         int *rnd = genRandomArray(size, min, max);
-        selection_sort(rnd, size);
+        Sort* selec = new SelectionSort();
+        selec->sort(rnd, size);
         assertAscending(rnd, size);
 
     }
 
+    //Best case analysis
+    {
+        Sort* insert = new BubbleSort();
+        Sort* select = new InsertionSort();
+        Sort* bubble = new SelectionSort();
+
+        insert->sort(
+                insert->genBstCase(size, min, max),
+                size
+        );
+
+        bubble->sort(
+                bubble->genBstCase(size, min, max),
+                size
+        );
+
+        select->sort(
+                select->genBstCase(size, min, max),
+                size
+        );
+
+        DUMP(insert->getCounter().assign);
+        DUMP(bubble->getCounter().assign);
+        DUMP(select->getCounter().assign);
+    }
+
+    //Worst case analysis
+    {
+        Sort* insert = new BubbleSort();
+        Sort* select = new InsertionSort();
+        Sort* bubble = new SelectionSort();
+
+        insert->sort(
+                insert->genWstCase(size, min, max),
+                size
+        );
+
+        bubble->sort(
+                bubble->genWstCase(size, min, max),
+                size
+        );
+
+        select->sort(
+                select->genWstCase(size, min, max),
+                size
+        );
+
+        DUMP(insert->getCounter().assign);
+        DUMP(bubble->getCounter().assign);
+        DUMP(select->getCounter().assign);
+    }
     return 0;
 }
 
 void print(int *array, int size) {
-    for(int i = 0; i<size; i++) {
+    for (int i = 0; i < size; i++) {
         DUMP(array[i]);
     }
 }
@@ -57,11 +241,14 @@ void print(int *array, int size) {
  *      if A[j] < A[j-1]
  *          exchange A[j] with A[j - 1]
  */
-void bubble_sort(int *A, int size) {
-    for(int i = 0; i<size-1; i++) {
-        for(int j = size; j>= i+1; j--) {
-            if(A[j]<A[j-1])
+void bubble_sort(int *A, int size, Count &count) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = size; j >= i + 1; j--) {
+            if (A[j] < A[j - 1]) {
+                count.compare++;
                 exchange(A[j], A[j - 1]);
+                count.assign += 3;
+            }
         }
     }
 }
@@ -74,6 +261,9 @@ void exchange(int &a, int &b) {
     b = aux;
 }
 
+
+
+
 /**
  * [2] "Introduction To Algorithms, 3rd ed." Pag. 18
  * for j = 2 to A.length
@@ -85,7 +275,7 @@ void exchange(int &a, int &b) {
  *          i = i - 1
  *      A[i+1] = key
  */
-void insertion_sort(int *A, int size) {
+void insertion_sort(int *A, int size, Count &counter) {
     int i = 0;
     for (int j = 1; j < size; j++) {
         int key = A[j];
@@ -93,8 +283,11 @@ void insertion_sort(int *A, int size) {
         while (i > -1 && A[i] > key) {      //be careful when adapting a [1 to n] algo to a [0 to n-1] one!
             A[i + 1] = A[i];
             i = i - 1;
+            counter.compare += 1;
+            counter.assign += 1;
         }
         A[i + 1] = key;
+        counter.assign += 2;
     }
 }
 
@@ -104,7 +297,7 @@ void insertion_sort(int *A, int size) {
  *      exchange the minimum found with the current one
  */
 void selection_sort(int *A, int size) {
-    for(int i = 0; i<size-1; i++) {
+    for (int i = 0; i < size - 1; i++) {
         int minim = indexOfMinimum(i, size, A);
         exchange(A[i], A[minim]);
     }
@@ -112,8 +305,8 @@ void selection_sort(int *A, int size) {
 
 int indexOfMinimum(int first, int end, int *A) {
     int min = first;
-    for(int i = first + 1; i < end; i++) {
-        if(A[i] < A[min]) {
+    for (int i = first + 1; i < end; i++) {
+        if (A[i] < A[min]) {
             min = i;
         }
     }
